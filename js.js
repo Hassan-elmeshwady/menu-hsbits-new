26a5fc399692a55339d9a70556d9928cbe23424e
Hassan-elmeshwady <hassan.elmeshwady@gmail.com>
2025-01-15 00:00:23 +0200

    mira

--- js.js
+++ js.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let currentIndex = 0;
 
     // Array of image sources
-    const images = ["image1.jpg", "image2.jpg" ,"image1.jpg", "image2.jpg" ,"image1.jpg", "image2.jpg" ,"image1.jpg", "image2.jpg" ,"image1.jpg", "image2.jpg" ,"image1.jpg", "image2.jpg" ,"image1.jpg", "image2.jpg" ,"image1.jpg", "image2.jpg" ,"image1.jpg", "image2.jpg" ,"image1.jpg", "image2.jpg" , ];
+    const images = ["image1.jpg", "image2.jpg" ,"image3.jpg", "image4.jpg" ,"image5.jpg", "image6.jpg" ,"image7.jpg", "image8.jpg" ,"image9.jpg", "image10.jpg" ,"image11.jpg", "image12.jpg" ,"image13.jpg", "image14.jpg" ,"image15.jpg", "image16.jpg" ,"image17.jpg", "image18.jpg" ,"image19.jpg", "image20.jpg" , "image21.jpg" , ];
 
     function showImage(index) {
         imageContainer.style.transform = `translateX(-${index * 100}%)`;
